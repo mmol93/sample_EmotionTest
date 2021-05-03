6cd346f9bf45bf6e44c3745f30340ee5b55619f6
ljws93 <ljws93@naver.com>
2021-05-03 14:28:35 +0900

    add nav_host_fragment in activity_main.xml

--- app/src/main/java/com/example/sample_emotiontest/MainActivity.kt
+++ app/src/main/java/com/example/sample_emotiontest/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.sample_emotiontest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.example.sample_emotiontest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
--- app/src/main/java/com/example/sample_emotiontest/fragment/MainFragment.kt
+++ app/src/main/java/com/example/sample_emotiontest/fragment/MainFragment.kt
@@ -17,4 +17,8 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
