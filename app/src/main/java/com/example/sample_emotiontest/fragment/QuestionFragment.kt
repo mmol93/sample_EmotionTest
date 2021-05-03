b872ec139bf6313324f5f2fb84132ed6fd277a3e
ljws93 <ljws93@naver.com>
2021-05-03 13:45:38 +0900

    built layouts

---
+++ app/src/main/java/com/example/sample_emotiontest/fragment/QuestionFragment.kt
@@ -0,0 +1,18 @@
+package com.example.sample_emotiontest.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sample_emotiontest.R
+
+class QuestionFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_question, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/sample_emotiontest/fragment/ResultFragment.kt
@@ -0,0 +1,19 @@
+package com.example.sample_emotiontest.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sample_emotiontest.R
+
+
+class ResultFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+}
